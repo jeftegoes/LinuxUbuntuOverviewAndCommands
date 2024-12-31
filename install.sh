a0ff8110894fa4fe6a7d8652ef5472670613ca9b
jeftegoes <jefte.goes.dev@outlook.com>
2024-12-30 23:29:35 -0300

    Updating script.

--- install.sh
+++ install.sh
@@ -70,12 +70,30 @@ snap install node --classic
 echo "Installing lm-sensors..."
 apt install lm-sensors
 
-# java openjdk 23   https://www.oracle.com/br/java/technologies/downloads/
-# edge https://www.microsoft.com/en-us/edge/business/download?form=MA13H4&cs=2899521137
+
 # docker ce
 # docker desktop
 # docker compose
-echo "Installing virtualbox and dependencies..."
+# https://docs.docker.com/engine/install/ubuntu/
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -Y
+
+# docker desktop - Prerequisites https://docs.docker.com/desktop/setup/install/linux/ubuntu/
+apt install gnome-terminal
+
+# java openjdk 23   https://www.oracle.com/br/java/technologies/downloads/
+# edge https://www.microsoft.com/en-us/edge/business/download?form=MA13H4&cs=2899521137
+echo "Installing virtual
+box and dependencies..."
 apt-get install virtualbox-ext-pack
 # virtualbox
 
